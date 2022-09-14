82a69458e1a3d99a4198cb7ad47da279851dedc6
dhewa.gunawan@gmail.com <dhewa.gunawan@gmail.com>
2022-09-14 15:53:53 +0700

    Add log debug to MainActivity

--- app/src/main/java/com/binar/binarch4/MainActivity.kt
+++ app/src/main/java/com/binar/binarch4/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(TAG, "First Time Using Android Studio")
+        Log.d(TAG, "BINAR TUTORIAL")
     }
 }
